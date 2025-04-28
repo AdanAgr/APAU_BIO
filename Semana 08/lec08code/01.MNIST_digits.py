f5b72dba33d95048b5941d5694d9eb0af41d7f38
saraporto <saraportoalvarez@gmail.com>
2025-04-28 08:35:00 +0200

    comentarios mnist

--- Semana 08/lec08code/01.MNIST_digits.py
+++ Semana 08/lec08code/01.MNIST_digits.py
@@ -1,48 +1,78 @@
-import torch
-from torch.utils.data import random_split, DataLoader
-from torchvision import datasets, transforms
-import sys
+###  ---------- IMPORTS ---------- ###
+import torch # librería deep learning
+from torch.utils.data import random_split, DataLoader # dividir datasets y cargarlos en "mini-lotes" (DataLoader).
+from torchvision import datasets, transforms # cargar datasets y transformar imágenes 
+import sys # para salir del programa si no hay GPU disponible
 
+
+###  ---------- CONFIGURACIÓN ---------- ###
+
+### CHECK GPU ###
+# Comprobar si hay GPU disponible
 if torch.cuda.is_available():
-    device = torch.device('cuda:0')
+    device = torch.device('cuda:0') # qué GPU usar
     print("GPU available:", torch.cuda.get_device_name(0))
 else:
     print("ERROR: no GPU available")
-    sys.exit(0)
+    sys.exit(0) # salir del programa si no hay GPU disponible
 
+
+
+### ---------- DATASET---------- ###
+
+### TRANSFORMACIONES ###
+# se aplican a cada imagen del dataset:
 custom_transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Lambda(lambda x: x.view(-1))
+    transforms.ToTensor(), # convierte la imagen a un tensor
+    transforms.Lambda(lambda x: x.view(-1)) # aplana imagen 28x28 (2D) a vector de 784 etos (1D)
 ])
 
+### CARGAR DATASET ###
+# se descarga en /tmp/data si no lo tienes + aplica las transformaciones
 mnist_dataset = datasets.MNIST(root='/tmp/data', download=True, transform = custom_transform)
-print("Length of dataset:", len(mnist_dataset))
-print("Length of first vector in dataset: ", mnist_dataset[0][0].shape)
-print("Label of first vector in dataset: ", mnist_dataset[0][1])
+print("Length of dataset:", len(mnist_dataset)) # nº ejemplos
+print("Length of first vector in dataset: ", mnist_dataset[0][0].shape) # shape del primer ejemplo (784,)
+print("Label of first vector in dataset: ", mnist_dataset[0][1]) # label del primer ejemplo (0-9)
 
-train_data, val_data = random_split(mnist_dataset, [50000, 10000])
+### DIVIDIR DATASET ###
+train_data, val_data = random_split(mnist_dataset, [50000, 10000]) # 50k train, 10k validation
+
 ## Print the length of train and validation datasets
 print("Length of Train Dataset: ", len(train_data))
 print("Length of Test Dataset: ", len(val_data))
 
-batch_size = 128
+batch_size = 128 # tamaño cada mini-lote (de 128 en 128 imágenes)
 
-train_loader = DataLoader(train_data, batch_size, shuffle = True, num_workers=2)
-val_loader = DataLoader(val_data, len(val_data) , shuffle = False, num_workers=2)
+train_loader = DataLoader(train_data, batch_size, shuffle = True, num_workers=2) # divide datasets en mini-lotes aleatorios 
+val_loader = DataLoader(val_data, len(val_data) , shuffle = False, num_workers=2) # carga todos los datos de validación de una vez
 
-net = torch.nn.Sequential(
-      torch.nn.Linear(784, 512),
-      torch.nn.ReLU(),
-      torch.nn.Linear(512, 10),
-      torch.nn.Softmax(dim=1)
+
+### ---------- RED NEURONAL ---------- ###
+
+### DEFINICIÓN RED ###
+net = torch.nn.Sequential( # modelo secuencial (los datos pasan de una capa a otra)
+      torch.nn.Linear(784, 512), # reducción de 784 entradas a 512 salidas
+      torch.nn.ReLU(), # función de activación ReLU (quita negativos, deja positivos)
+      torch.nn.Linear(512, 10), # reducción de 512 entradas a 10 salidas (una para cada dígito)
+      torch.nn.Softmax(dim=1) # función de activación Softmax (normaliza las salidas para que sumen 1 --> probabilidades)
       ).to(device)
 
-print(net)
+print(net) # imprime la red neuronal
 
+
+### ---------- ENTRENAMIENTO ---------- ###
+
+# entrenar el modelo durante 50 ciclos completos (epochs) sobre todos los datos
 num_epochs = 50
 
-optimizer = torch.optim.RMSprop(net.parameters(), lr = 0.001)
+# optimizador RMSprop, que ajusta los pesos
+optimizer = torch.optim.RMSprop(net.parameters(), lr = 0.001) 
+
+# función de pérdida (CrossEntropyLoss) para clasificación multiclase
 criterion = torch.nn.CrossEntropyLoss()
+
+
+### BUCLE DE ENTRENAMIENTO ###
 
 for epoch in range(num_epochs):
     net.train()
